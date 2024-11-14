859a7da34d89af14a54ba8021226f80b28e9d54a
popcon1234 <popcon1234@naver.com>
2024-11-14 17:29:05 +0900

    init

---
+++ src/main/java/com/kdg/jpa_schedule/JpaScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.kdg.jpa_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaScheduleApplication.class, args);
+    }
+
+}
